1515ba30e0929445e271cdb4f989cbb44254c854
Denis Blokhin <denblo@yandex-team.ru>
2021-10-25 12:21:14 +0300

    file relocations

---
+++ lua/plugins.lua
@@ -0,0 +1,101 @@
+local cwd = vim.fn.expand('<sfile>:p:h')
+
+local rtp =  vim.api.nvim_get_option('packpath')
+rtp = rtp .. "," .. cwd
+vim.api.nvim_set_option('packpath', rtp)
+
+local util = require 'packer.util'
+local packer = require 'packer' 
+
+packer.init({
+	package_root = util.join_paths(cwd, 'pack'),
+	compile_path = util.join_paths(cwd, 'lua', 'packer_compiled.lua'),
+})
+
+local function prequire(...)
+	local status, lib = pcall(require, ...)
+	if (status) then return lib end
+	return nil
+end
+
+packer.startup(function()
+	use 'wbthomason/packer.nvim'
+
+	use 'scrooloose/nerdtree'
+	use 'NLKNguyen/papercolor-theme'
+	use { 
+		'sonph/onehalf', 
+		rtp = 'vim/'
+	}
+
+	use 'tpope/vim-surround'
+	use 'tpope/vim-commentary'
+
+	use 'terryma/vim-multiple-cursors'
+	use 'Raimondi/delimitMate'
+
+	use 'francoiscabrol/ranger.vim'
+	use 'rbgrouleff/bclose.vim'
+
+	use {
+		'famiu/feline.nvim',
+		config = function() require'feline'.setup() end
+	}
+
+	use { 
+		'nvim-telescope/telescope.nvim',
+		config = function()
+			local actions = require("telescope.actions")
+
+			require("telescope").setup({
+				defaults = {
+					mappings = {
+						i = {
+							["<ESC>"] = actions.close,
+						},
+					},
+				},
+			})
+		end	
+	}
+	use 'nvim-lua/popup.nvim'
+	use 'nvim-lua/plenary.nvim'
+
+	use 'L3MON4D3/LuaSnip'
+	use { 
+		'hrsh7th/nvim-cmp',
+		config = function()
+			require'cmp'.setup {
+				snippet = {
+					expand = function(args)
+						require'luasnip'.lsp_expand(args.body)
+					end
+				},
+
+				sources = {
+					{ name = 'luasnip' },
+				},
+			}
+		end
+	}
+	use { 'saadparwaiz1/cmp_luasnip' }
+
+	use { 
+		'neovim/nvim-lspconfig',
+		config = function()
+			local nvim_lsp = require("lspconfig")
+			nvim_lsp.tsserver.setup {
+				root_dir = function()
+					return vim.fn.getcwd()
+				end,
+				on_attach = function(client)
+					client.resolved_capabilities.document_formatting = false
+					on_attach(client)
+				end,
+			}
+		end
+	}
+
+end)
+
+prequire('packer_compiled')
---
+++ pack/base/start/config/lua/settings/keys.lua
@@ -0,0 +1,66 @@
+local function map(mode, lhs, rhs, opts)
+	local options = {noremap = true}
+	if opts then options = vim.tbl_extend('force', options, opts) end
+	vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+end
+
+map('n', '<F1>', ":NERDTreeToggle<CR>") 
+map('n', '<A-1>', ":NERDTreeFind<CR>") 
+map('n', '<F2>', ":Telescope file_browser<CR>") 
+map('n', '<F3>', ":Telescope find_files<CR>") 
+map('n', '<F4>', ":Telescope buffers<CR>") 
+map('n', '<F5>', ":Telescope live_grep<CR>") 
+
+map('n', '<F7>', ":new term://zsh<CR><C-w>Ji") 
+map('n', '<A-2>', ":Ranger<CR>") 
+map('t', '<Esc>', "<C-\\><C-n>") 
+map('n', '<C-k>', ":Commentary<CR>") 
+map('v', '<C-k>', ":Commentary<CR>") 
+
+local cmp = require'cmp'
+
+local t = function(str)
+       return vim.api.nvim_replace_termcodes(str, true, true, true)
+end
+
+local check_back_space = function()
+       local col = vim.fn.col('.') - 1
+       if col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
+               return true
+       else
+               return false
+       end
+end
+
+local luasnip = require('luasnip')
+
+_G.tab_complete = function()
+	print("tab_complte")
+	if vim.fn.pumvisible() == 1 then
+		return t "<C-n>"
+	elseif luasnip and luasnip.expand_or_jumpable() then
+		return t("<Plug>luasnip-expand-or-jump")
+	elseif check_back_space() then
+		return t "<Tab>"
+	elseif cmp.visible() then
+		return ""
+	end
+	return t "<Tab>"
+end
+_G.s_tab_complete = function()
+	if vim.fn.pumvisible() == 1 then
+		return t "<C-p>"
+	elseif luasnip and luasnip.jumpable(-1) then
+		return t("<Plug>luasnip-jump-prev")
+	else
+		return t "<S-Tab>"
+	end
+	return ""
+end
+
+vim.api.nvim_set_keymap("i", "<Tab>", "v:lua.tab_complete()", {expr = true})
+vim.api.nvim_set_keymap("s", "<Tab>", "v:lua.tab_complete()", {expr = true})
+-- vim.api.nvim_set_keymap("i", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
+-- vim.api.nvim_set_keymap("s", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
+vim.api.nvim_set_keymap("i", "<C-E>", "<Plug>luasnip-next-choice", {})
+vim.api.nvim_set_keymap("s", "<C-E>", "<Plug>luasnip-next-choice", {})
